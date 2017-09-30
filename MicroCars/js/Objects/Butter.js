3df6f2d7258db097c7049de3bd3c1cb8b6c82b81
Keyaku <antonio.luis.sarmento@gmail.com>
2017-09-30 19:02:32 +0100

    Added simple Butter object

---
+++ MicroCars/js/Objects/Butter.js
@@ -0,0 +1,31 @@
+class Butter {
+	constructor(x, y, z) {
+		this.type = 'Butter';
+		this.mesh = new THREE.Object3D();
+
+		// FIXME: Apply different textures to different sides
+		var tex = TextureLoader.load('butter_side.png');
+		var material = new THREE.MeshBasicMaterial({ map: tex });
+
+		var pos = new THREE.Vector3(0, 0, 0)
+		var size = new THREE.Vector3(7, 10, 20);
+		new ButterBox(this.mesh, material, pos, size);
+
+		this.mesh.position.set(x, y, z);
+		scene.add(this.mesh);
+		return this.mesh;
+	}
+}
+
+class ButterBox {
+	constructor(obj, material, pos, size) {
+		this.type = 'ButterBox';
+
+		var geometry = new THREE.BoxGeometry(size.x, size.y, size.z);
+		var mesh = new THREE.Mesh(geometry, material);
+		mesh.position.copy(pos);
+
+		obj.add(mesh);
+		return mesh;
+	}
+}
