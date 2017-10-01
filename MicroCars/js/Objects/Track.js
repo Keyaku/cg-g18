c717e332121b563bded9c94d4a6cbf0623b034d7
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2017-10-01 08:55:48 +0100

    Track.js - comments and improved code

--- MicroCars/js/Objects/Track.js
+++ MicroCars/js/Objects/Track.js
@@ -1,37 +1,31 @@
 class Track {
 	constructor() {
-		this.type = 'Track';
-		this.mesh = new THREE.Object3D();
+		this.type = 'Track'
+		this.mesh = new THREE.Object3D()
 
-		// trackWidth takes an int value that defines how large the road is
-		var trackWidth = 45;
-		// These points are used to generate the track and it's aspect
+		//Sets how wide the track is.
+		var trackWidth = 45
+		//Sets the position of the curves that compose the track.
 		var points = [
-			{x:0, z:0},
-			{x:200, z:0},
-			{x:280, z:80},
-			{x:800, z:80},
-			{x:800, z:400},
-			{x:400, z:1000},
-			{x:200, z:1000},
-			{x:240, z:600},
-			{x:400, z:480},
-			{x:400, z:200},
-			{x:140, z:200},
-			{x:140, z:540},
-			{x:100, z:1000},
-			{x:0, z:1000},
-			{x:0, z:600},
-			{x:80, z:480},
-			{x:0, z:400},
-			{x:0, z:200},
+			{x:0, z:0}, {x:200, z:0},
+			{x:280, z:80}, {x:800, z:80},
+			{x:800, z:400}, {x:400, z:1000},
+			{x:200, z:1000}, {x:240, z:600},
+			{x:400, z:480}, {x:400, z:200},
+			{x:140, z:200}, {x:140, z:540},
+			{x:100, z:1000}, {x:0, z:1000},
+			{x:0, z:600}, {x:80, z:480},
+			{x:0, z:400}, {x:0, z:200},
 			//{x:0, z:0},
-		];
+		]
+		//Offsets the points to be alligned with the camera and converts them 
+		//to be THREE.Vector3 to be used by THREE.CatmullRomCurve3.
 		points = this.pointsOffset(points, 400)
-		points = this.pointsConvertToVector3(points)
+		//Draws the track.
 		var track = this.trackCreate(this.mesh, points, trackWidth, 0x13294B)
+		//Adds the tori on the track.
 		this.trackAddTorus(this.mesh, track.geometry.vertices)
-
+		//Adds all to the scene.
 		scene.add(this.mesh)
 		return this
 	}
@@ -40,19 +34,12 @@ class Track {
 		var p2 = []
 		for (var i = 0; i < points.length; i++) {
 			var p = points[i]
-			p2.push({x: p.x - offset, z: p.z - offset})
-		}
-		return p2
-	}
-	pointsConvertToVector3(points) {
-		var p2 = []
-		for (var i = 0; i < points.length; i++) {
-			var p = points[i]
-			p2.push(new THREE.Vector3(p.x, 0, p.z))
+			p2.push(new THREE.Vector3(p.x - offset, 0, p.z - offset))
 		}
 		return p2
 	}
 	trackCreate(obj, points, width, color) {
+		//Creates a Curve to be the shape of the track defined by the points.
 		var closedSpline = new THREE.CatmullRomCurve3(points)
 		closedSpline.type = 'catmullrom'
 		closedSpline.closed = true
@@ -60,25 +47,43 @@ class Track {
 			new THREE.Vector2(0, -width / 2),
 			new THREE.Vector2(0, width / 2),
 			new THREE.Vector2(1, 0),
-		]);
+		])
+		//Extrusion settings.
+		//Steps represents the smoothness of the corners.
+		//ExtrudePath extrudes along the curve defined above. This is what gives
+		//the width to the track (instead of being a line).
 		var extrudeSettings = {steps:2000, bevelEnabled:false, extrudePath:closedSpline}
-		var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-		var material = new THREE.MeshLambertMaterial({color:color})
+		//Creates the track geometry.
+		var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings)
+		//Creates the track material.
+		var material = new THREE.MeshBasicMaterial({color:color})
 		var mesh = new THREE.Mesh(geometry, material)
+		//Adds the mesh to the track class object.
 		obj.add(mesh)
 		return mesh
 	}
 	trackAddTorus(obj, vertices) {
+		//Number of vertices in the tracks's outside line.
 		var verticesOut = 4010
+		//Number of vertices in the tracks's inside line.
 		var verticesIn = 1992
+		/*
+		The spacing of the tori is different in the inside and outside
+		because there are more outside vertices than inside ones. For the 
+		tori to be spaces equally on both sides of the track the spacing ratio
+		needs to match the vertices amount ratio.
+		*/
+		//Step of the for loop for the tori to be added on the inside line.
 		var inSpacing = 6
+		//Step of the for loop for the tori to be added on the outside line.
 		var outSpacing = parseInt(inSpacing / (verticesIn / verticesOut))
+		//Adds the tires to the track's outside line.
 		for (var i = 0; i < verticesOut; i += outSpacing)
 			new Tire(obj, vertices[i])
+		//Adds the tires to the track's inside line.
 		for (var i = verticesOut; i < vertices.length; i += inSpacing)
 			new Tire(obj, vertices[i])
 	}
-
 }
 
 class Tire {
@@ -110,11 +115,16 @@ class Tire {
 			this.mesh.add(mesh)
 		}
 		*/
+		//Creates the torus geometry.
 		var geometry = new THREE.TorusGeometry(2.5, 0.8, 5, 16)
+		//Creates the torus material.
 		var material = new THREE.MeshBasicMaterial({color:0xAA1111})
 		var mesh = new THREE.Mesh(geometry, material)
+		//Positions the torus to be on the track point.
 		mesh.position.set(p.x, p.y, p.z)
+		//Rotates the torus to be horizontal.
 		mesh.rotation.set(3.14 / 2, 0, 0)
+		//Adds the mesh to the track class object.
 		this.mesh.add(mesh)
 		obj.add(this.mesh)
 	}
