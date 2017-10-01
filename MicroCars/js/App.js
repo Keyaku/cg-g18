7f51bdee0ed2a8a2f796a9f2ea192169c8e11a5b
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2017-10-01 04:00:42 +0000

    Lots of sweat and tears. Camera done.

--- MicroCars/js/App.js
+++ MicroCars/js/App.js
@@ -1,39 +1,44 @@
 /**
 * edibleObjects is a dictionary with interactable objects such as oranges.
 */
+var controls;
+var gameBoard, raceTrack, car;
+var stats;
+var renderer, scene, camera;
 var edibleObjects = {};
-var board, track, car;
-var renderer, scene, camera, controls;
+const frustumSize = 1000;
 
 /**
 * This method creates a perspective camera. Switching between perspective and
 * orthographic cameras is also possible due to event listeners.
 */
 this.createCamera = function() {
+	var aspectRatio = window.innerWidth / window.innerHeight;
 	if (camera instanceof THREE.PerspectiveCamera) {
-		// OrthographicCamera params: Left, Right, Top, Bottom, Near, Far
-		// FIXME @FranciscoKlogan
-		camera = new THREE.OrthographicCamera( - window.innerWidth / 2, window.innerWidth / 2, window.innerHeight / 2, - window.innerHeight / 2, 0.01, 1250);
+		var left	 	= (frustumSize * aspectRatio) / (-2);
+		var right 	= (frustumSize * aspectRatio) / 2;
+		var top 		= frustumSize / 2;
+		var bottom 	= frustumSize / (-2);
+		camera = new THREE.OrthographicCamera(left, right, top, bottom, 0.1, 2000 );
 		camera.position.x = 0;
-		camera.position.y = 1200;
+		camera.position.y = 1000;
 		camera.position.z = 0;
-		camera.lookAt(scene.position);
 		this.perspective = "Orthographic";
-		controls = new THREE.OrbitControls(camera);
+		camera.lookAt(scene.position);
 	} else {
 		// PerspectiveCamera params: Field of View, Aspect Ratio, Near, Far
-		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1800);
-		camera.position.x = -150;
-		camera.position.y = 200;
-		camera.position.z = -150;
+		camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1800);
+		camera.position.x = -250;
+		camera.position.y = 600;
+		camera.position.z = -250
 		camera.lookAt(scene.position);
 		this.perspective = "Perspective";
-		controls = new THREE.OrbitControls(camera);
 	}
+	controls = new THREE.OrbitControls(camera);
 };
 
 /**
-* Self calling method that allows the rendering to be handled by the system.
+* Render method allows system to handle all the rendering.
 */
 function render() {
 	renderer.render(scene, camera);
@@ -41,34 +46,9 @@ function render() {
 	requestAnimationFrame(render);
 }
 
-/**
-* Create scene instanciates the board, the track, the car and several other
-* objects as scene graph.
-*/
-function createScene() {
-	scene = new THREE.Scene();
-	// The X axis is red, Y is green and Z is blue.
-	scene.add(new THREE.AxisHelper(600));
-	board = new Board(0, -10, 0);
-	track = new Track();
-	car = new Car(20, 2, -5);
-	createEdible('FirstOrange', 200, 5, 40);
-	createEdible('SecondOrange', 100, 5, 80);
-	createEdible('ThirdOrange', 0, 5, 500);
-	/**
-	// TODO @Keyaku
-	createEdible('FirstButter', 0, 5, 0);
-	createEdible('SecondButter', 0, 5, 0);
-	createEdible('ThirdButter', 0, 5, 0);
-	createEdible('FourthButter', 0, 5, 0);
-	createEdible('FifthButter', 0, 5, 0);
-	*/
-}
-
 /*******************************************************************************
-* Helper methods
+* Object Oriented - Helper methods
 *******************************************************************************/
-
 /**
 * Adds a new edible of type Object3D with subtype Orange or Butter to a dictionary
 * whose keys are the desired Object3D name passed as a String
@@ -91,18 +71,90 @@ function deleteEdible(edibleName) {
 	delete edibleObjects[edibleName];
 }
 
+/*******************************************************************************
+* Scene related - Helper methods
+*******************************************************************************/
 /**
-* TODO on a later date, the current lightning was copy&pasted from the example
-* of the first class.
+* Instanciates the Scene with Axis & Grid helpers to assist devs with coordinates.
+* Generates the board, representing the game world where other Oject3D objects are held.
+*	Instanciates and adds edibles to the track which debuff the player when touched
+* Object3D of subtype Car to a dictionary
 */
-function createLights() {
-	var sunLight = new THREE.DirectionalLight(0xffffff, 1);
-	var hemisphereLight = new THREE.HemisphereLight( 0xAEEEEE, 0xFFFFFF, 0.6 );
-	sunLight.position.set(0, 0, 600).normalize();
-	hemisphereLight.position.set( 0, 500, 0 );
-	scene.add(new THREE.AmbientLight(0xFFFFFF, 0.2));
+function createScene() {
+	var size = 1000;
+	var divisions = 100;
+	scene = new THREE.Scene();
+	// The X axis is red, Y is green and Z is blue.
+	// scene.add(new THREE.GridHelper(size, divisions) );
+	scene.add(new THREE.AxisHelper(size / 2));
+	scene.background = new THREE.Color(0xAEEEEE);
+	gameBoard = new Board(0, -10, 0);
+	raceTrack = new Track();
+	car = new Car(20, 2, -5);
+	createEdible('FirstOrange', 200, 5, 40);
+	createEdible('SecondOrange', 100, 5, 80);
+	createEdible('ThirdOrange', 0, 5, 500);
+	/**
+	// TODO @Keyaku
+	createEdible('FirstButter', 0, 5, 0);
+	createEdible('SecondButter', 0, 5, 0);
+	createEdible('ThirdButter', 0, 5, 0);
+	createEdible('FourthButter', 0, 5, 0);
+	createEdible('FifthButter', 0, 5, 0);
+	*/
+}
+
+/*******************************************************************************
+* Light related - Helper methods
+*******************************************************************************/
+
+/**
+* Adds a soft white light with default intensity to the scene.
+*/
+function createAmbient() {
+	var ambientLight = new THREE.AmbientLight(0x404040);
+	ambientLight.name = 'ambientLight';
+	scene.add(ambientLight);
+}
+
+/**
+* A light source positioned directly above the scene, color fades from the sky
+* towards the ground. Simulates horizon.
+*/
+function createHorizon() {
+	var horizonLight = new THREE.HemisphereLight(0xFF4444, 0x44FF44, 0.6);
+	horizonLight.name = 'horizonLight';
+	horizonLight.position.set( 0, frustumSize, 0 );
+	scene.add(horizonLight);
+}
+
+/**
+* A light that uses parallel rays allowing to simulate sunlight, it is the only
+* light used here that allows shadow casting.
+*/
+function createSun() {
+	var sunLight = new THREE.DirectionalLight(0xFFFFE0);
+	sunLight.name = 'sunLight';
+	sunLight.position.x = Math.random()*100 + 800;
+	sunLight.position.y = Math.random()*100 + 800;
+	sunLight.position.z = Math.random()*100 + 800;
+	sunLight.castShadow = true;
+	sunLight.shadowCameraVisible = false;
+	sunLight.shadowCameraNear = 1;
+	sunLight.shadowCameraFar = frustumSize;
+	sunLight.shadowCameraLeft = -frustumSize / 2;
+	sunLight.shadowCameraRight = frustumSize / 2;
+	sunLight.shadowCameraTop = frustumSize / 2;
+	sunLight.shadowCameraBottom = -frustumSize / 2;
+	sunLight.shadowMapWidth = 1000;
+	sunLight.shadowMapHeight = 1000;
 	scene.add(sunLight);
-	scene.add(hemisphereLight);
+}
+// Calls all other light instanciating methods
+function createLights() {
+	createAmbient();
+	createHorizon();
+	createSun();
 }
 
 /**
@@ -116,11 +168,14 @@ function init() {
 	'use strict';
 	renderer = new THREE.WebGLRenderer({antialias:true});
 	renderer.setSize(window.innerWidth, window.innerHeight);
+	// Background color
+	renderer.setClearColor(0x6698FF, 1);
+	renderer.shadowMapEnabled = true;
 	document.body.appendChild(renderer.domElement);
 	createScene();
 	createLights();
 	createCamera();
 	render();
-	window.addEventListener('resize', onResize);
+	window.addEventListener('resize', onWindowResize, false);
 	window.addEventListener('keydown', onKeyDown);
 }
--- MicroCars/js/EventHandler.js
+++ MicroCars/js/EventHandler.js
@@ -1,28 +1,39 @@
-function onResize() {
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	if (window.innerHeight > 0 && window.innerWidth > 0) {
-		camera.aspect = renderer.getSize().width / renderer.getSize().height;
+function onWindowResize() {
+	var windowHeight = window.innerHeight;
+	var windowWidth = window.innerWidth;
+	var aspectRatio = windowWidth / windowHeight;
+	if (windowWidth > 0 && windowHeight > 0) {
+		if (camera instanceof THREE.PerspectiveCamera) {
+			renderer.setSize(window.innerWidth, window.innerHeight);
+			camera.aspect = renderer.getSize().width / renderer.getSize().height;
+		} else {
+			camera.left   = - frustumSize * aspectRatio / 2;
+			camera.right  =   frustumSize * aspectRatio / 2;
+			camera.top    =   frustumSize / 2;
+			camera.bottom = - frustumSize / 2;
+		}
 		camera.updateProjectionMatrix();
+		renderer.setSize(windowWidth, windowHeight);
 	}
 }
 
 function onKeyDown(e) {
 	switch(e.keyCode) {
-	case 65: //A
-	case 97: //a
-		scene.traverse(function (node) {
-			if (node instanceof THREE.Mesh)
-				node.material.wireframe = !node.material.wireframe;
-		});
-		break;
-	case 83: //S
-	case 115: //s
-		createCamera();
-		break;
-	case 37: //left arrow
-	case 38: //up arrow
-	case 39: //right arrow
-	case 40: //down arrow
+		case 65: //A
+		case 97: //a
+			scene.traverse(function (node) {
+				if (node instanceof THREE.Mesh)
+					node.material.wireframe = !node.material.wireframe;
+			});
+			break;
+		case 83: //S
+		case 115: //s
+			createCamera();
+			break;
+		case 37: //left arrow
+		case 38: //up arrow
+		case 39: //right arrow
+		case 40: //down arrow
 		break;
 	}
 }
