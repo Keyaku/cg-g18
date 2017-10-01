64665b8fc61f310c8c940310ffb092efd2281e72
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2017-10-01 07:56:42 +0100

    Revert "Improved Track.js code"
    This reverts commit b3e17d0fab8af3175167780c3c62f5f3b7db5dac.

--- MicroCars/js/Objects/Track.js
+++ MicroCars/js/Objects/Track.js
@@ -27,56 +27,38 @@ class Track {
 			{x:0, z:200},
 			//{x:0, z:0},
 		];
-		points = this.pointsOffset(points, 400)
-		points = this.pointsConvertToVector3(points)
-		var track = this.trackCreate(this.mesh, points, trackWidth, 0x13294B)
-		this.trackAddTorus(track.geometry.vertices)		
-
-		scene.add(this.mesh)
-		return this
-	}
-
-	pointsOffset(points, offset) {
-		var p2 = []
 		for (var i = 0; i < points.length; i++) {
 			var p = points[i]
-			p2.push({x: p.x - offset, z: p.z - offset})
+			points[i] = new THREE.Vector3(p.x, 0, p.z)
 		}
-		return p2
-	}
-	pointsConvertToVector3(points) {
-		var p2 = []
-		for (var i = 0; i < points.length; i++) {
-			var p = points[i]
-			p2.push(new THREE.Vector3(p.x, 0, p.z))
-		}
-		return p2
-	}
-	trackCreate(obj, points, width, color) {
 		var closedSpline = new THREE.CatmullRomCurve3(points)
 		closedSpline.type = 'catmullrom'
 		closedSpline.closed = true
+
 		var shape = new THREE.Shape([
-			new THREE.Vector2(0, -width / 2),
-			new THREE.Vector2(0, width / 2),
+			new THREE.Vector2(0, -trackWidth / 2),
+			new THREE.Vector2(0, trackWidth / 2),
 			new THREE.Vector2(1, 0),
 		]);
 		var extrudeSettings = {steps:2000, bevelEnabled:false, extrudePath:closedSpline}
 		var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-		var material = new THREE.MeshLambertMaterial({color:color})
-		var mesh = new THREE.Mesh(geometry, material)
-		obj.add(mesh)
-		return mesh
-	}
-	trackAddTorus(obj, vertices) {
+		var material = new THREE.MeshLambertMaterial({color:0x13294B})
+		var track = new THREE.Mesh(geometry, material)
+
+		this.mesh.add(track)
+
+		var vertices = track.geometry.vertices
 		var verticesOut = 4010
 		var verticesIn = 1992
 		var inSpacing = 6
 		var outSpacing = parseInt(inSpacing / (verticesIn / verticesOut))
-		for (var i = 0; i < verticesOut; i += outSpacing)
-			new Tire(obj, 'black', vertices[i])
-		for (var i = verticesOut; i < vertices.length; i += inSpacing)
-			new Tire(obj, 'white', vertices[i])
+		for (i = 0; i < verticesOut; i += outSpacing)
+			new Tire(this.mesh, 'black', vertices[i])
+		for (i = verticesOut; i < vertices.length; i += inSpacing)
+			new Tire(this.mesh, 'white', vertices[i])
+
+		scene.add(this.mesh)
+		return this
 	}
 }
 
@@ -108,6 +90,7 @@ class Tire {
 			mesh.rotation.set(3.14 / 2, 0, 0)
 			this.mesh.add(mesh)
 		}
+
 		obj.add(this.mesh)
 	}
 }
