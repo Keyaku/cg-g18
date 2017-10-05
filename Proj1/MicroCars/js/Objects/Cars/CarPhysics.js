f05a87335350d66e7ab11e2860c1081571b67f62
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2017-10-05 18:58:37 +0100

    CarPhysics.js - removed comments and cleaned code

--- Proj1/MicroCars/js/Objects/Cars/CarPhysics.js
+++ Proj1/MicroCars/js/Objects/Cars/CarPhysics.js
@@ -3,23 +3,12 @@ const ACCELERATION = 2;
 var friction = 0.02;
 
 class CarPhysics {
-	constructor(x, y, z, carHeading) {
-		this.lastPosition = new THREE.Vector3(x, y, z);
-
+	constructor(carHeading) {
 		this.heading = carHeading.clone();
 		this.velocity = 0;
+		this.currentAcceleration = 0;
 		this.forwardAcceleration = ACCELERATION;
 		this.decayAcceleration = ACCELERATION / 4;
-
-		this.currentAcceleration = 0;
-
-
-		//Movement type: Uniformly Accelerated Motion
-		//Law of motion: s(t) = s0 + v0*t + 0.5*a*t^2
-		//Considering s0 = 0, v0 = 0
-		//Updated law of motion: 	s(t) = 0.5*a*t^2
-		//							s(t2) = s(t1) + 0.5*a*(t2^2 - t1^2)
-		//Displacement: s(t2) - s(t1) = 0.5*a*(t2^2 - t1^2)
 	}
 
 	update(delta) {
@@ -43,10 +32,8 @@ class CarPhysics {
 			[0		0		1] * [1]
 			*/
 			var x = this.heading.x, z = this.heading.z
-			var cos = Math.cos(angle);
-			var sin = Math.sin(angle);
-			var xNew = cos * x - sin * z;
-			var zNew = sin * x + cos * z;
+			var cos = Math.cos(angle), sin = Math.sin(angle);
+			var xNew = cos * x - sin * z, zNew = sin * x + cos * z;
 			this.heading.set(xNew, 0, zNew);
 			car.mesh.rotateY(-angle);
 		}
