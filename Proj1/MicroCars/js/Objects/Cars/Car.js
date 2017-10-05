ecc205dcd8cd4d50d271f37eba8bdc5aba2dd034
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2017-10-05 11:08:02 +0100

    Car.js - added car physics object to Car

--- Proj1/MicroCars/js/Objects/Cars/Car.js
+++ Proj1/MicroCars/js/Objects/Cars/Car.js
@@ -1,61 +1,69 @@
 class Car {
 	constructor(x, y, z) {
-		this.type = 'Car'
-		this.mesh = new THREE.Object3D()
+		this.type = 'Car';
+		this.mesh = new THREE.Object3D();
 		//Creates the material for the car body.
 		var matBody = new THREE.MeshPhongMaterial({
 			color:0x2194ce, /*Blue*/ specular: 0x111111, shininess: 10,
-		})
+		});
 		//Creates the material for the axles.
-		var materialAxle = new THREE.MeshBasicMaterial({color:0x960101})
+		var materialAxle = new THREE.MeshBasicMaterial({color:0x960101});
 		//Extrude setting that define the car width.
-		var extrSettings = {amount:8, bevelEnabled:false}
+		var extrSettings = {amount:8, bevelEnabled:false};
 		//Creates the body panels.
-		var c1 = new CarSquare(this.mesh, matBody, extrSettings, 3, 5, 0, 0)
-		var c2 = new CarSquare(this.mesh, matBody, extrSettings, 8, 3, 3, 2)
-		var c3 = new CarSquare(this.mesh, matBody, extrSettings, 7, 2, 11, 0)
-		var c5 = new CarSquare(this.mesh, matBody, extrSettings, 12, 6, 11, 2)
-		var t1 = new CarTriangle(this.mesh, matBody, extrSettings, -2, 2.5, 0, 0, 2.5, 0, 0, 5, 0)
-		var t2 = new CarTriangle(this.mesh, matBody, extrSettings, -2, 2.5, 0, 0, 0, 0, 0, 2.5, 0)
-		var t3 = new CarTriangle(this.mesh, matBody, extrSettings, 3, 0, 0, 4, 2, 0, 3, 2, 0)
-		var t4 = new CarTriangle(this.mesh, matBody, extrSettings, 9, 2, 0, 11, 0, 0, 11, 2, 0)
-		var t5 = new CarTriangle(this.mesh, matBody, extrSettings, 8.5, 5, 0, 11, 5, 0, 11, 8, 0)
-		var t6 = new CarTriangle(this.mesh, matBody, extrSettings, 18, 0, 0, 19, 2, 0, 18, 2, 0)
-		var t7 = new CarTriangle(this.mesh, matBody, extrSettings, 22, 2, 0, 23, 0, 0, 23, 2, 0)
-		var t8 = new CarTriangle(this.mesh, matBody, extrSettings, 23, 0, 0, 24, 5, 0, 23, 5, 0)
-		var t9 = new CarTriangle(this.mesh, matBody, extrSettings, 23, 5, 0, 24, 5, 0, 23, 8, 0)
+		var c1 = new CarSquare(this.mesh, matBody, extrSettings, 3, 5, 0, 0);
+		var c2 = new CarSquare(this.mesh, matBody, extrSettings, 8, 3, 3, 2);
+		var c3 = new CarSquare(this.mesh, matBody, extrSettings, 7, 2, 11, 0);
+		var c5 = new CarSquare(this.mesh, matBody, extrSettings, 12, 6, 11, 2);
+		var t1 = new CarTriangle(this.mesh, matBody, extrSettings, -2, 2.5, 0, 0, 2.5, 0, 0, 5, 0);
+		var t2 = new CarTriangle(this.mesh, matBody, extrSettings, -2, 2.5, 0, 0, 0, 0, 0, 2.5, 0);
+		var t3 = new CarTriangle(this.mesh, matBody, extrSettings, 3, 0, 0, 4, 2, 0, 3, 2, 0);
+		var t4 = new CarTriangle(this.mesh, matBody, extrSettings, 9, 2, 0, 11, 0, 0, 11, 2, 0);
+		var t5 = new CarTriangle(this.mesh, matBody, extrSettings, 8.5, 5, 0, 11, 5, 0, 11, 8, 0);
+		var t6 = new CarTriangle(this.mesh, matBody, extrSettings, 18, 0, 0, 19, 2, 0, 18, 2, 0);
+		var t7 = new CarTriangle(this.mesh, matBody, extrSettings, 22, 2, 0, 23, 0, 0, 23, 2, 0);
+		var t8 = new CarTriangle(this.mesh, matBody, extrSettings, 23, 0, 0, 24, 5, 0, 23, 5, 0);
+		var t9 = new CarTriangle(this.mesh, matBody, extrSettings, 23, 5, 0, 24, 5, 0, 23, 8, 0);
 		//Creates the wheels.
-		var w1 = new CarTorus(this.mesh, 2.5, 1.7, 10, 16, 5.5, -1, -2)
-		var w2 = new CarTorus(this.mesh, 2.5, 1.7, 10, 16, 20.5, -1, -2)
-		var w3 = new CarTorus(this.mesh, 2.5, 1.7, 10, 16, 5.5, -1, 10)
-		var w4 = new CarTorus(this.mesh, 2.5, 1.7, 10, 16, 20.5, -1, 10)
+		var rfw = new CarTorus(this.mesh, 2.5, 1.7, 10, 16, 5.5, -1, -2);
+		var rrw = new CarTorus(this.mesh, 2.5, 1.7, 10, 16, 20.5, -1, -2);
+		var lfw = new CarTorus(this.mesh, 2.5, 1.7, 10, 16, 5.5, -1, 10);
+		var lrw = new CarTorus(this.mesh, 2.5, 1.7, 10, 16, 20.5, -1, 10);
 		//Creates the axles.
-		var cy1 = new CarCylinder(this.mesh, materialAxle, 0.5, 12, 8, 1, 5.5, -1, 4, 90, 0, 0)
-		var cy2 = new CarCylinder(this.mesh, materialAxle, 0.5, 12, 8, 1, 20.5, -1, 4, 90, 0, 0)
-		var cy3 = new CarCylinder(this.mesh, materialAxle, 0.5, 3, 8, 1, 5.5, 0.5, 4, 0, 0, 0)
-		var cy4 = new CarCylinder(this.mesh, materialAxle, 0.5, 3, 8, 1, 20.5, 0.5, 4, 0, 0, 0)
-		var cy5 = new CarCylinder(this.mesh, materialAxle, 0.5, 15, 8, 1, 13, -1, 4, 0, 0, 90)
+		var cy1 = new CarCylinder(this.mesh, materialAxle, 0.5, 12, 8, 1, 5.5, -1, 4, 90, 0, 0);
+		var cy2 = new CarCylinder(this.mesh, materialAxle, 0.5, 12, 8, 1, 20.5, -1, 4, 90, 0, 0);
+		var cy3 = new CarCylinder(this.mesh, materialAxle, 0.5, 3, 8, 1, 5.5, 0.5, 4, 0, 0, 0);
+		var cy4 = new CarCylinder(this.mesh, materialAxle, 0.5, 3, 8, 1, 20.5, 0.5, 4, 0, 0, 0);
+		var cy5 = new CarCylinder(this.mesh, materialAxle, 0.5, 15, 8, 1, 13, -1, 4, 0, 0, 90);
 		//Positions the car.
-		this.mesh.position.set(x, y, z)
+		this.mesh.position.set(x, y, z);
 		//Rotates the car.
-		this.mesh.rotation.set(0, 3.14, 0)
+		this.mesh.rotation.set(0, 3.14, 0);
 		//Scales the car.
-		this.mesh.scale.set(0.5, 0.5, 0.5)
+		this.mesh.scale.set(0.5, 0.5, 0.5);
 
-		//Creates the car physics
-		var carPhysics = new CarPhysics(2)
-		this.carPhysics = carPhysics
 
-		var p1 = w1.position
-		var p2 = w2.position
-		var fv = new THREE.Vector3(p2.x - p1.x, 0, p2.z - p1.z)
-		this.forwardVector = fv.normalize()
-
+		var p1 = rfw.position;
+		var p2 = rrw.position;
+		var fv = new THREE.Vector3(p2.x - p1.x, 0, p2.z - p1.z);
+		this.forwardVector = fv.normalize();
+		this.carPhysics = new CarPhysics(x, y, z, this.forwardVector);
+		this.carControls = {
+			forward:0,
+			backward:0,
+			left:0,
+			rigth:0
+		};
 
 		//Adds the car to the scene.
-		scene.add(this.mesh)
+		scene.add(this.mesh);
 		return this
 	}
+
+	update(time, delta) {
+		this.carPhysics.update(time, delta);
+	}
+	
 }
 
 class CarTriangle {
