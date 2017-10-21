7e8dd2269a5ce39aedf1f91ac9f37e9e62e5cd17
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2017-10-21 21:19:10 +0000

    Global.js: Spawn classmethods are now functions

--- Proj1/MicroCars/js/Core/Globals.js
+++ Proj1/MicroCars/js/Core/Globals.js
@@ -26,3 +26,52 @@ const Z_AXIS_HEADING = new THREE.Vector3(0, 0, 1);
 *******************************************************************************/
 const TO_DEGREES = 180 / Math.PI;
 const TO_RADIANS = Math.PI / 180;
+const NINETY_DEGREES = Math.PI / 2;
+
+/*******************************************************************************
+* Helper methods
+*******************************************************************************/
+
+/**
+* objectNeedsRespawn verifies if an object is within the boundaries of the board
+* @param x: x position of the object subject to verification.
+* @param z: z position of the object subject to verification.
+* @return: Boolean value True if orange is outside of the board. False otherwise
+*/
+function objectNeedsRespawn(x, z) {
+  if (x >= ((-1) * HALF_BOARD_WIDTH) && x <= (HALF_BOARD_WIDTH) &&
+      z >= ((-1) * HALF_BOARD_LENGHT) && z <= (HALF_BOARD_LENGHT)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/** generateSpawnLocation(min, max)
+@param min: used to calculate a random coordinate in inverval [min, max]
+@param max: used to calculate a random coordinate in inverval [min, max]
+@var signX: defines if x coordinate is positive or negative
+@var signZ: defines if z coordinate is positive or negative
+@var x: X coordinate (value calculated randomly based on min-max values)
+@var y: Y coordiante (default is 0 - orange stay on top of the board at all times)
+@var z: Z coordinate (value calculated randomly based on min-max values)
+@return: Vector3D that defines a new spawn location after orange falls from the table.
+*/
+function generateSpawnLocation(min, max) {
+  var signX = Math.random() < 0.5 ? -1 : 1;
+  var signZ = Math.random() < 0.5 ? -1 : 1;
+  var x = Math.floor(Math.random() * (max - min + 1)) + min;
+  var y = 0;
+  var z = Math.floor(Math.random() * (max - min + 1)) + min;
+  var spawnLocation = new THREE.Vector3(x, y, z);
+  return spawnLocation;
+}
+
+/** respawnObject(spawnLocation, axis, distance)
+* @param obj: object that is being respawned
+* @param distance: How far should the orange travel after respawning
+*/
+function respawnObject(obj, distance) {
+  obj.position.set(generateSpawnLocation(0, HALF_BOARD_WIDTH));
+  obj.move(X_AXIS_HEADING, distance);
+}
--- Proj1/MicroCars/js/Objects/Orange.js
+++ Proj1/MicroCars/js/Objects/Orange.js
@@ -1,3 +1,34 @@
+class OrangeWrapper extends MotionBody {
+	constructor(orangeName, x, y, z, radius = 5) {
+
+	}
+	
+	update(delta) {
+		var angular_velocity = 5 / this.radius;								// var angular_velocity = this.collisionData.velocity / this.radius;
+		var movement_direction = new THREE.Vector3(1, 0, 0); 	// var movement_direction = this.collisionData.direction;
+		var x = movement_direction.x;
+		var y = movement_direction.y;
+		var z = movement_direction.z;
+		var rotation_vector = new THREE.Vector3(x, y, z)
+
+		var translation = 15 * delta;
+		this.translateOnAxis(movement_direction, translation);
+		// rotation_vector.applyAxisAngle(Y_AXIS_HEADING, theta);
+		// NOTE: Axis deve ser perpendicular ao movimento e paralelo ao chão
+		// this.rotateOnAxis(rotation_vector, theta);
+	}
+
+	move(axis, distance) {
+		var colliding = super.move(axis, distance);
+		this.translateOnAxis(axis, distance);
+		return colliding;
+	}
+}
+
+/*******************************************************************************
+* Concrete Orange class
+*******************************************************************************/
+
 class Orange extends MotionBody {
 	constructor(orangeName, x, y, z, radius = 5) {
 		super(0.140);
@@ -10,90 +41,29 @@ class Orange extends MotionBody {
 		scene.add(this);
 		return this;
 	}
-
-	update(delta) {
-		// var movement_direction = this.collisionData.direction; NOTE: use Object3D.getWorldDirection()
-		// var angular_velocity = this.collisionData.velocity / this.radius;
-		var angular_velocity = 15 / this.radius;
-		var theta = angular_velocity * delta;
-
-		// axis perpendicular ao movimento e paralelo ao chao Oxz
-		// this.rotateOnAxis(/*TODO*/, delta)
-	}
-
-
-	move(axis, distance) {
-		var colliding = super.move(axis, distance);
-		this.translateOnAxis(axis, distance);
-		return colliding;
-	}
-
-
-	/**
-	* orangeNeedsRespawn verifies if <this> orange is within the boundaries of the board
-	* @return: Boolean value True if orange is outside of the board. False otherwise
-	*/
-	orangeNeedsRespawn() {
-		// NOTE: This code might not work because the orange isnt in board coordinates but in scene coordinates
-		var x = this.position.x;
-		var z = this.position.z;
-		if (x >= ((-1) * HALF_BOARD_WIDTH) && x <= (HALF_BOARD_WIDTH) &&
-				z >= ((-1) * HALF_BOARD_LENGHT) && z <= (HALF_BOARD_LENGHT)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	/** respawnOrange(spawnLocation, axis, distance)
-	@param spawnLocation: Vector3D that represents the respawn point on the board
-	@param axis: Axis along which the orange should move after respawning
-	@param distance: How far should the orange travel after respawning
-	@return: null
-	*/
-	respawnOrange(spawnLocation, axis, distance) {
-		this.position.set(spawnLocation);
-		this.move(X_AXIS_HEADING, distance);
-	}
-
-	/** generateSpawnLocation(min, max)
-	@param min: used to calculate a random coordinate in inverval [min, max]
-	@param max: used to calculate a random coordinate in inverval [min, max]
-	@var signX: defines if x coordinate is positive or negative
-	@var signZ: defines if z coordinate is positive or negative
-	@var x: X coordinate (value calculated randomly based on min-max values)
-	@var y: Y coordiante (default is 0 - orange stay on top of the board at all times)
-	@var z: Z coordinate (value calculated randomly based on min-max values)
-	@return: Vector3D that defines a new spawn location after orange falls from the table.
-	*/
-	generateSpawnLocation(min, max) {
-		var signX = Math.random() < 0.5 ? -1 : 1;
-		var signZ = Math.random() < 0.5 ? -1 : 1;
-		var x = Math.floor(Math.random() * (max - min + 1)) + min;
-		var y = 0;
-		var z = Math.floor(Math.random() * (max - min + 1)) + min;
-		var spawnLocation = new THREE.Vector3(x, y, z);
-		return spawnLocation;
-	}
 }
 
-class OrangeLeaf {
-	constructor(obj, x, y, z) {
-		this.type = 'OrangeLeaf';
+/*******************************************************************************
+* OrangeFruit class
+*******************************************************************************/
+
+class OrangeFruit {
+	constructor(obj, x, y, z, radius = 5) {
+		this.type = 'OrangeFruit';
 		this.mesh = new THREE.Object3D();
-		var leafShape = new THREE.Shape();
-		leafShape.moveTo( 0, 0 );
-		leafShape.bezierCurveTo( x + 6, y - 6, x + 12, y + 4, x, y );
-		var extrudeSettings = { amount: 0.5, bevelEnabled: false };
-		var geometry = new THREE.ExtrudeGeometry( leafShape, extrudeSettings );
-		var material = new THREE.MeshLambertMaterial({color: 0x009900});
-		var mesh = new THREE.Mesh( geometry, material );
+		var material = new THREE.MeshLambertMaterial({color:0xFF9900});
+		var geometry = new THREE.SphereGeometry(radius, 15, 15);
+		var mesh = new THREE.Mesh(geometry, material);
 		this.mesh.add(mesh);
-		this.mesh.position.set(x, y+6.5, z);
+		this.mesh.position.set(x, y, z);
 		obj.add(this.mesh);
 		return this.mesh;
 	}
 }
+
+/*******************************************************************************
+* OrangeBranch class
+*******************************************************************************/
 
 class OrangeBranch {
 	constructor(obj, x, y, z) {
@@ -109,15 +79,23 @@ class OrangeBranch {
 	}
 }
 
-class OrangeFruit {
-	constructor(obj, x, y, z, radius = 5) {
-		this.type = 'OrangeFruit';
+/*******************************************************************************
+* OrangeLeaf class
+*******************************************************************************/
+
+class OrangeLeaf {
+	constructor(obj, x, y, z) {
+		this.type = 'OrangeLeaf';
 		this.mesh = new THREE.Object3D();
-		var material = new THREE.MeshLambertMaterial({color:0xFF9900});
-		var geometry = new THREE.SphereGeometry(radius, 15, 15);
-		var mesh = new THREE.Mesh(geometry, material);
+		var leafShape = new THREE.Shape();
+		leafShape.moveTo( 0, 0 );
+		leafShape.bezierCurveTo( x + 6, y - 6, x + 12, y + 4, x, y );
+		var extrudeSettings = { amount: 0.5, bevelEnabled: false };
+		var geometry = new THREE.ExtrudeGeometry( leafShape, extrudeSettings );
+		var material = new THREE.MeshLambertMaterial({color: 0x009900});
+		var mesh = new THREE.Mesh( geometry, material );
 		this.mesh.add(mesh);
-		this.mesh.position.set(x, y, z);
+		this.mesh.position.set(x, y+6.5, z);
 		obj.add(this.mesh);
 		return this.mesh;
 	}
