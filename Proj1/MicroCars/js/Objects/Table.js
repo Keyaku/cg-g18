cfd94732d48ad29b2ce7dd73499163acd63a2e0c
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2017-10-26 22:13:14 +0000

    Table.js: OK

--- Proj1/MicroCars/js/Objects/Table.js
+++ Proj1/MicroCars/js/Objects/Table.js
@@ -1,41 +1,49 @@
-class Table {
+class Table extends THREE.Object3D{
 	constructor(x=0, y=0, z=0, size=BOARD_WIDTH) {
-		this.type = 'Table';
-		this.mesh = new THREE.Object3D();
+		super();
 
-		var material = new THREE.MeshLambertMaterial({color:0xDEB887});
+		this.type = 'Table';
+		this.position.set(x, y, z);
+
 		var halfSize = size >> 1;
 		var legDistance = size >> 2;
 		var thickness = 5;
 
-		new TableTop(this.mesh, material, 0, -thickness, 0, size, thickness);
-		new TableLeg(this.mesh, material, -legDistance, -1, -legDistance, halfSize);
-		new TableLeg(this.mesh, material, -legDistance, -1, legDistance, halfSize);
-		new TableLeg(this.mesh, material, legDistance, -1, legDistance, halfSize);
-		new TableLeg(this.mesh, material, legDistance, -1, -legDistance, halfSize);
+		new TableTop(this, 0, -thickness, 0, size, thickness);
+		new TableLeg(this, -legDistance, -1, -legDistance, halfSize);
+		new TableLeg(this, -legDistance, -1, legDistance, halfSize);
+		new TableLeg(this, legDistance, -1, legDistance, halfSize);
+		new TableLeg(this, legDistance, -1, -legDistance, halfSize);
 
-		this.mesh.position.set(x, y, z);
-		scene.add(this.mesh);
+		scene.add(this);
+
 		return this;
 	}
 }
 
-class TableTop {
-	constructor(obj, material, x, y, z, size, thickness) {
+class TableTop extends THREE.Mesh {
+	constructor(obj, x, y, z, size, thickness) {
 		var geometry = new THREE.CubeGeometry(size, thickness, size);
-		var mesh = new THREE.Mesh(geometry, material);
-		mesh.position.set(x, y, z);
-		obj.add(mesh);
-		return mesh;
+		super(geometry);
+		this.lambertMaterial = new THREE.MeshLambertMaterial({color:0xDEB887});
+		this.phongMaterial = new THREE.MeshPhongMaterial({color:0xDEB887});
+		this.material = this.lambertMaterial;
+		this.type = 'TableTop';
+		this.position.set(x, y, z);
+		obj.add(this);
+		return this;
 	}
 }
 
-class TableLeg {
-	constructor(obj, material, x, y, z, size) {
+class TableLeg extends THREE.Mesh  {
+	constructor(obj, x, y, z, size) {
 		var geometry = new THREE.CubeGeometry(size/10, size, size/10);
-		var mesh = new THREE.Mesh(geometry, material);
-		mesh.position.set(x, y - (size >> 1), z);
-		obj.add(mesh);
-		return mesh;
+		super(geometry);
+		this.lambertMaterial = new THREE.MeshLambertMaterial({color:0xDEB887});
+		this.phongMaterial = new THREE.MeshPhongMaterial({color:0xDEB887});
+		this.material = this.lambertMaterial;
+		this.position.set(x, y - (size >> 1), z);
+		obj.add(this);
+		return this;
 	}
 }
