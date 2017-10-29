f48cbdc8d77e106bb04979d09045c8ffb0e0101f
Keyaku <antonio.luis.sarmento@gmail.com>
2017-10-29 18:23:15 +0000

    Added barebones for BoundingBox

--- Proj1/MicroCars/js/Objects/BoundingGeometry.js
+++ Proj1/MicroCars/js/Objects/BoundingGeometry.js
@@ -3,37 +3,29 @@
 * Attaches a bounding box to a given mesh with option to toggle visibility or not
 *******************************************************************************/
 
-class BoundingSphere extends THREE.Mesh {
-	constructor(mesh, radius=0, center=new THREE.Vector3()) {
+/*
+**	BoundingGeometry
+** Abstract class for BoundingGeometries.
+** Do NOT instance one directly.
+*/
+class BoundingGeometry extends THREE.Mesh {
+	constructor(mesh) {
 		super();
-		this.type = "BoundingSphere";
+		this.type = "BoundingGeometry";
 		this.visible = false;
-
-		if (radius <= 0) { // This generates automatically a sphere around our mesh
-			this.setFromMesh(mesh);
-		} else { // This goes straight to the point and creates the sphere according to our radius
-			this.updateBounds(radius, center)
-		}
 
 		this.material = new THREE.MeshBasicMaterial({wireframe:true});
 		this.scale.copy(mesh.scale);
 		return this;
 	}
 
-	updateBounds(radius, center=this.position) {
-		this.geometry = new THREE.SphereBufferGeometry(radius, 8, 8);
-		this.position.copy(center);
-
-		this.geometry.boundingSphere = new THREE.Sphere(center, radius);
-
-		this.radius = radius;
+	// Lifesavers
+	toggleVisibility() {
+		this.visible = !this.visible;
 	}
 
-	// Mesh-altering methods
-	setFromMesh(obj) {
-		var min = new THREE.Vector3(0, 0, 0);
-		var max = new THREE.Vector3(1, 0, 0);
-
+	// Self mesh-altering methods
+	setFromMesh(obj, min=new THREE.Vector3(), max=new THREE.Vector3(1)) {
 		var vertices;
 		// Checking mesh's own geometry (if available)
 		if (obj.geometry != undefined) {
@@ -57,21 +49,86 @@ class BoundingSphere extends THREE.Mesh {
 				}
 			}
 		}
+	}
+}
+
+/*
+**	BoundingBox
+** The AABB of BoundingGeometries.
+** Requires an update every time the geometry rotates.
+*/
+class BoundingBox extends BoundingGeometry {
+	constructor(mesh, min=undefined, max=undefined) {
+		super(mesh);
+
+		if (min == undefined || max == undefined) {
+			this.setFromMesh(mesh, min, max);
+		} else {
+			this.updateBounds(min, max);
+		}
+
+		return this;
+	}
+
+	setFromMesh(obj, min, max) {
+		super.setFromMesh(obj);
+		this.updateBounds(min, max);
+	}
+
+	// Updates geometry and boundaries
+	updateBounds(min=new THREE.Vector3(), max=new THREE.Vector3(1,1,1)) {
+		var diagonal = max.clone().sub(min);
+		this.geometry = new THREE.BoxBufferGeometry(diagonal.x, diagonal.y, diagonal.z);
+		this.geometry.boundingBox = new THREE.Box3(min, max);
+	}
+}
+
+/*
+**	BoundingSphere
+** Sphere of a radius that is supposed to surround the entire object.
+** Better suited for performance since it doesn't need updating.
+** Less suited for proper collisions as it is less accurate.
+*/
+class BoundingSphere extends BoundingGeometry {
+	constructor(mesh, radius=0, center=new THREE.Vector3()) {
+		super(mesh);
+		this.type = "BoundingSphere";
+
+		if (radius <= 0) { // This generates automatically a sphere around our mesh
+			this.setFromMesh(mesh);
+		} else { // This goes straight to the point and creates the sphere according to our radius
+			this.updateBounds(radius, center)
+		}
+
+		return this;
+	}
+
+	// Self mesh-altering methods
+	setFromMesh(obj) {
+		var min = new THREE.Vector3(0, 0, 0);
+		var max = new THREE.Vector3(1, 0, 0);
+
+		super.setFromMesh(obj, min, max);
 
 		var radius = min.distanceTo(max) / 2;
 		this.updateBounds(radius, new THREE.Vector3());
 	}
 
-	// Lifesavers
-	toggleVisibility() {
-		this.visible = !this.visible;
+	// Updates geometry and boundaries
+	updateBounds(radius, center=this.position) {
+		this.position.copy(center);
+
+		this.geometry = new THREE.SphereBufferGeometry(radius, 8, 8);
+		this.geometry.boundingSphere = new THREE.Sphere(center, radius);
+
+		this.radius = radius;
 	}
 
 	// Collision methods
 	intersects(bounds) {
-		if (bounds.type != this.type) {
-			return false; // We don't support other BoundingGeometries *yet*
-		}
+		if (!(bounds instanceof BoundingSphere)) {
+			return false;
+		} // FIXME: do this for BoundingBox whenever possible
 
 		var sum = this.radius + bounds.radius;
 		var d = this.getWorldPosition().clone();
