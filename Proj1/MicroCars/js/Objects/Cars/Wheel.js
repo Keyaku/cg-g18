ab9ab4e3fc74842a2011b74c4b657043425ade4e
Keyaku <antonio.luis.sarmento@gmail.com>
2017-10-29 18:32:50 +0000

    Wheel: Refactored code

--- Proj1/MicroCars/js/Objects/Cars/Wheel.js
+++ Proj1/MicroCars/js/Objects/Cars/Wheel.js
@@ -7,37 +7,54 @@ class Wheel extends THREE.Geometry {
     this.bladeWidth       = wheelradius / 6;
     this.capInnerLenght   = Math.sqrt(2*(this.bladeWidth*this.bladeWidth))
 
-    // Define wheel's centercap points and spike point
-    this.vertices.push(new THREE.Vector3(-this.capInnerLenght, 0, 0));     // 0
-    this.vertices.push(new THREE.Vector3(this.capInnerLenght, 0, 0));      // 1
-    this.vertices.push(new THREE.Vector3(0, this.capInnerLenght, 0));      // 2
-    this.vertices.push(new THREE.Vector3(0, -this.capInnerLenght, 0));     // 3
+    // Defining the geometry's vertices
+	var vertices = [
+		// Define wheel's centercap points and spike point
+		[-this.capInnerLenght, 0, 0],
+		[this.capInnerLenght, 0, 0],
+		[0, this.capInnerLenght, 0],
+		[0, -this.capInnerLenght, 0],
+		[-this.capInnerLenght, 0, -depth],
+		[this.capInnerLenght, 0, -depth],
+		[0, this.capInnerLenght, -depth],
+		[0, -this.capInnerLenght, -depth],
+		[0, 0, this.capInnerLenght],
+	];
 
-    this.vertices.push(new THREE.Vector3(-this.capInnerLenght, 0, -depth)); // 4
-    this.vertices.push(new THREE.Vector3(this.capInnerLenght, 0, -depth));  // 5
-    this.vertices.push(new THREE.Vector3(0, this.capInnerLenght, -depth));  // 6
-    this.vertices.push(new THREE.Vector3(0, -this.capInnerLenght, -depth)); // 7
+	// Pushing the vertices in
+	for (var i in vertices) {
+		var vertex = vertices[i];
+		this.vertices.push(new THREE.Vector3(vertex[0], vertex[1], vertex[2]));
+	}
 
-    this.vertices.push(new THREE.Vector3(0, 0, this.capInnerLenght)); // 8
-    // Define wheel's centercap front and back faces
-    this.faces.push(new THREE.Face3(0, 2, 3));
-    this.faces.push(new THREE.Face3(1, 2, 3));
-    this.faces.push(new THREE.Face3(4, 6, 7));
-    this.faces.push(new THREE.Face3(5, 6, 7));
-    // Define wheel's centercap lateral faces left to right, top to bottom
-    this.faces.push(new THREE.Face3(2, 4, 6));
-    this.faces.push(new THREE.Face3(0, 2, 4));
-    this.faces.push(new THREE.Face3(4, 0, 7));
-    this.faces.push(new THREE.Face3(0, 3, 7));
-    this.faces.push(new THREE.Face3(3, 5, 7));
-    this.faces.push(new THREE.Face3(3, 4, 5));
-    this.faces.push(new THREE.Face3(2, 5, 6));
-    this.faces.push(new THREE.Face3(2, 4, 5));
-    // Define spike faces
-    this.faces.push(new THREE.Face3(0, 2, 8));
-    this.faces.push(new THREE.Face3(0, 3, 8));
-    this.faces.push(new THREE.Face3(2, 4, 8));
-    this.faces.push(new THREE.Face3(3, 4, 8));
+	// Defining faces
+	var faces = [
+		// Define wheel's centercap front and back faces
+		[0, 2, 3],
+		[1, 2, 3],
+		[4, 6, 7],
+		[5, 6, 7],
+		// Define wheel's centercap lateral faces left to right, top to bottom
+		[2, 4, 6],
+		[0, 2, 4],
+		[4, 0, 7],
+		[0, 3, 7],
+		[3, 5, 7],
+		[3, 4, 5],
+		[2, 5, 6],
+		[2, 4, 5],
+		// Define spike faces
+		[0, 2, 8],
+		[0, 3, 8],
+		[2, 4, 8],
+		[3, 4, 8],
+	];
+
+	// Pushing the faces in
+	for (var i in faces) {
+		var face = faces[i];
+		this.faces.push(new THREE.Face3(face[0], face[1], face[2]));
+	}
 
     this.computeFaceNormals();
     this.computeVertexNormals();
