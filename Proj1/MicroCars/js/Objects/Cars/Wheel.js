19e59561d20e114a26694cb3c88ec21b9e2cd36e
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2017-11-01 16:40:40 +0000

    Wheel: Restart point

--- Proj1/MicroCars/js/Objects/Cars/Wheel.js
+++ Proj1/MicroCars/js/Objects/Cars/Wheel.js
@@ -30,24 +30,24 @@ class Wheel extends THREE.Geometry {
 	// Defining faces
 	var faces = [
 		// Define wheel's centercap front and back faces
-		[0, 2, 3],
-		[1, 2, 3],
-		[4, 6, 7],
-		[5, 6, 7],
+		[0, 2, 3],        // 0
+		[1, 2, 3],        // 1
+		[4, 6, 7],        // 2
+		[5, 6, 7],        // 3
 		// Define wheel's centercap lateral faces left to right, top to bottom
-		[2, 4, 6],
-		[0, 2, 4],
-		[4, 0, 7],
-		[0, 3, 7],
-		[3, 5, 7],
-		[3, 4, 5],
-		[2, 5, 6],
-		[2, 4, 5],
+		[2, 4, 6],        // 4
+		[0, 2, 4],        // 5
+		[4, 0, 7],        // 6
+		[0, 3, 7],        // 7
+		[3, 5, 7],        // 8
+		[3, 4, 5],        // 9
+		[2, 5, 6],        // 10
+		[2, 4, 5],        // 11
 		// Define spike faces
-		[0, 2, 8],
-		[0, 3, 8],
-		[2, 4, 8],
-		[3, 4, 8],
+		[0, 2, 8],        // 12
+		[0, 3, 8],        // 13
+		[2, 4, 8],        // 14
+		[3, 4, 8],        // 15
 	];
 
 	// Pushing the faces in
