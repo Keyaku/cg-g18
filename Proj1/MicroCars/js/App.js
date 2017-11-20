9b1f60ba41baceeb5010b89464888ff7ce6edd70
Keyaku <antonio.luis.sarmento@gmail.com>
2017-11-20 16:40:50 +0000

    Adding mrdoob's Stats
    Changes:
    - Added Stats (for FPS counting and more)
    - Separated CSS stylesheet from HTML file.

--- Proj1/MicroCars/js/App.js
+++ Proj1/MicroCars/js/App.js
@@ -1,6 +1,7 @@
 /**
 * This is the 'main' JS file. Everything down to globals and runtime happens here.
 */
+var stats;
 var renderer, cameraManager, controls;
 var lightManager;
 var game, gameBoard, raceTrack, car;
@@ -40,6 +41,9 @@ function render() {
 * Animate updates all necessary data, and calls the rendering logic
 */
 function animate() {
+	// Starting frame monitor
+	stats.begin();
+
 	// Animation and physics updates to all visible PhysicsBody
 	var delta = clock.getDelta();
 
@@ -53,6 +57,9 @@ function animate() {
 
 	render(); // Rendering
 	cleanQueue(); // Freeing queued Object3D from scene
+
+	// Ending frame monitor
+	stats.end();
 
 	// Requesting a call to this function
 	requestAnimationFrame(animate);
@@ -73,6 +80,10 @@ function init() {
 	renderer.setClearColor(0x6698FF, 1);
 	renderer.shadowMap.enabled = true;
 	document.body.appendChild(renderer.domElement);
+
+	stats = new Stats();
+	stats.showPanel(0);
+	document.getElementById("benchmark").appendChild(stats.dom);
 
 	createScene();
 	game = new Game(5);
